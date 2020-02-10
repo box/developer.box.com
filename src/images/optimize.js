b19b6278ad003beaf81190e87b9d81809342eb20
Cristiano Betta <cbetta@box.com>
2020-02-10 15:46:52 +0000

    Add image preoptimization script

---
+++ src/images/optimize.js
@@ -0,0 +1,35 @@
+const sharp = require(`sharp`)
+const glob = require(`glob`)
+const fs = require(`fs-extra`)
+
+// Configure the desired quality
+const MAX_WIDTH = 1800
+const QUALITY = 80
+
+// Find all images
+const filenames = glob.sync(`compiled/**/*.{png,jpg,jpeg}`)
+
+Promise.all(
+  filenames.map(async filename => {
+    // Create a sharp stream
+    const stream = sharp(filename)
+
+    // Get the file's metadata
+    const info = await stream.metadata()
+
+    // Return if the file is too small
+    if (info.width < MAX_WIDTH) { return }
+
+    // Create an optimized filename
+    const tmpName = filename.replace(
+      /(\..+)$/,
+      (_, ext) => `-optimized${ext}`
+    )
+    await stream
+      .resize(MAX_WIDTH)
+      .jpeg({ quality: QUALITY })
+      .toFile(tmpName)
+
+    return fs.rename(tmpName, filename)
+  })
+)
